b395aaa89272f23bea30ab445d4316f041fcc78e
Joan Tomas <joan.tomas.miralles@gmail.com>
2026-07-07 01:01:51 +0200

    Aplicar cambios de scroll y Navbar en la raiz (la que usa Cloudflare Pages)

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,13 +3,19 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
+    <nav aria-label="Main navigation">
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/stations/lofi">Lofi</Link>
         </li>
         <li>
-          <Link to="/blog">Blog</Link>
+          <Link to="/stations/jazz">Jazz</Link>
+        </li>
+        <li>
+          <Link to="/stations/synthwave">Synthwave</Link>
+        </li>
+        <li>
+          <Link to="/stations/nature">Nature</Link>
         </li>
       </ul>
     </nav>
