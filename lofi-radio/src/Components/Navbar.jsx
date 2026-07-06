861e990c9425c20768ccb43751d1d039f232566b
Joan Tomas <joan.tomas.miralles@gmail.com>
2026-07-07 01:04:57 +0200

    Quitar Navbar completamente

--- lofi-radio/src/Components/Navbar.jsx
+++ lofi-radio/src/Components/Navbar.jsx
@@ -1,25 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Navbar() {
-  return (
-    <nav aria-label="Main navigation">
-      <ul>
-        <li>
-          <Link to="/stations/lofi">Lofi</Link>
-        </li>
-        <li>
-          <Link to="/stations/jazz">Jazz</Link>
-        </li>
-        <li>
-          <Link to="/stations/synthwave">Synthwave</Link>
-        </li>
-        <li>
-          <Link to="/stations/nature">Nature</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+  return null;
 }
 
 export default Navbar;
